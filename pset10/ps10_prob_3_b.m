9f5ec02f851f887855b94202981099672a21c3c8
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-05-05 00:37:31 -0400

    more work on problem 3

--- pset10/ps10_prob_3_b.m
+++ pset10/ps10_prob_3_b.m
@@ -1,11 +1,20 @@
 syms w_0;
+L = sym('L', [3 1]);
+syms a x;
+
+% set this to true if you want to do h2syn
+if true
+  w_0 = 1;
+  a = 1;
+  s = tf('s');
+end
+
 A = [0 0 0; 0 0 1; 1 -w_0^2 0];
 B = [1 0; 0 0; 0 0];
 C = [0 1 0];
 D = [0 1];
 eps = 1e-5;
-L = sym('L', [3 1]);
-syms a x s;
+
 poly = charpoly(A + L*C, x)
 L_test = [-a^3; -3*a; (w_0^2 - 3*a^2) ];
 eig(A + L_test*C)
@@ -13,6 +22,8 @@ eig(A + L_test*C)
 C_0 = [1 0 0; 0 1 0];
 B_0 = -L_test;
 A_0 = A + L_test*C;
+
+
 
 F_0 = C_0*(s*eye(3) - A_0)^(-1)*B_0;
 F_0 = simplify(F_0);
@@ -29,6 +40,14 @@ P(2,1) = -1/(s*(s^2+w_0^2));
 P(2,4) = 1;
 P(3,1:2) = P_f_to_w_g;
 
-P = simplify(P)
+P = simplify(P);
+
+% make this into TITO system
+NMEAS = 1; % dim(y)
+NCON = 2; % dim(u)
+P_tito = mktito(P, NMEAS, NCON);
+
+% now we can try to do h2syn on this system
+
 
 
